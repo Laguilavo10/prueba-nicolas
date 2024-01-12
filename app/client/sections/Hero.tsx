48790d1b22ccb2235ecd62d078c356cad44007a2
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2024-01-12 13:15:29 -0500

    Add responsive to Hero section

--- app/client/sections/Hero.tsx
+++ app/client/sections/Hero.tsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <section className='bg-[url(/bkg_hero.png)] w-full flex text-white flex-col justify-center items-center h-[874px] bg-cover bg-no-repeat bg-bottom rounded-3xl'>
-      <h1 className='text-6xl text-center'>Navega las aguas <br/>cristalinas de Bariloche</h1>
-      <p className='text-xl mt-3'>
+    <section className='bg-[url(/images/bkg_hero.png)]  flex text-white flex-col justify-center items-center h-[874px] bg-cover bg-no-repeat bg-bottom rounded-3xl md:mx-20 mx-4 px-4'>
+      <h1 className='text-center text-4xl md:text-6xl '>Navega las aguas <br/>cristalinas de Bariloche</h1>
+      <p className='mt-3 text-center text-base md:text-xl'>
         Reserva tu bote y deja que Bariloche sea el escenario de tu próxima gran
         historia{' '}
       </p>
-      <button className='bg-principal p-4 rounded-3xl mt-9'>Contactar ahora</button>
+      <button className='bg-principal p-4 rounded-3xl mt-9 active:scale-95'>Contactar ahora</button>
     </section>
   )
 }
