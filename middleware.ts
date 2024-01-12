23fea8aff8811e253c8921e8e8dece0e0017f33b
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2024-01-12 15:27:52 -0500

    Configure Middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const pathname = req.nextUrl.pathname
+
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { session }
+  } = await supabase.auth.getSession()
+
+  // If the user is authenticated and is trying to access the login page, redirect to the home page.
+  if (session && pathname === '/signin') {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // If the user is not authenticated and is trying to access any page other than the login page, redirect to the login page.
+  if (!session && pathname !== '/signin') {
+    return NextResponse.redirect(new URL('/signin', req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: [
+    '/',
+    '/signin',
+  ]
+}
